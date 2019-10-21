4dfeeecc7797d87b5f98069722d6786d00459aa5
pointdisarr <56577702+pointdisarr@users.noreply.github.com>
2019-10-21 15:29:27 +0400

    NameConvention
    -classnames

--- src/main/java/shootinTheSquare.java
+++ src/main/java/shootinTheSquare.java
@@ -1,7 +1,7 @@
 import java.util.Arrays;
 import java.util.Scanner;
 
-public class be2homework2 {
+public class shootinTheSquare {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
